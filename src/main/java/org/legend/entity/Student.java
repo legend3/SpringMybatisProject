e607af6f71f7832ec85651e0bc8a9dd669a9fa45
legend3 <zhenguo50@126.com>
2021-10-21 05:19:56 +0800

    init commit

---
+++ src/main/java/org/legend/entity/Student.java
@@ -0,0 +1,31 @@
+package org.legend.entity;
+
+public class Student {
+    private int stuAge;
+    private String sutName;
+    private int stuNotes;
+
+    public int getStuAge() {
+        return stuAge;
+    }
+
+    public void setStuAge(int stuAge) {
+        this.stuAge = stuAge;
+    }
+
+    public String getSutName() {
+        return sutName;
+    }
+
+    public void setSutName(String sutName) {
+        this.sutName = sutName;
+    }
+
+    public int getStuNotes() {
+        return stuNotes;
+    }
+
+    public void setStuNotes(int stuNotes) {
+        this.stuNotes = stuNotes;
+    }
+}
