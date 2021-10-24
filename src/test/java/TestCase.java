69f8f7eb52c87804e48ed14816d4c0f24e44aa38
legend3 <zhenguo50@126.com>
2021-10-24 18:04:22 +0800

    springMybatisProject完结

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -1,17 +1,22 @@
+
 import org.legend.springMybatis.entity.Student;
 import org.legend.springMybatis.service.IStudentService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.Test;
 
-public class Test {
-    public static void main(String[] args) {
+public class TestCase {
+    @Test
+    public void case01() {
+        //加载spring容器文件
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //service层操作数据库事务
         IStudentService studentService = (IStudentService)context.getBean("studentService") ;
         Student student = new Student();
-        student.setStuAge(333);
-        student.setStuName("33s9");
-        student.setStuNo(339);
-        studentService.addStudent(student);
+        student.setStuAge(18);
+        student.setStuName("长沙");
+        student.setStuNo(8);
+        studentService.add(student);
 
     }
 }
